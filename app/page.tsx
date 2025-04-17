440d8fba22d9eb85643a17d0860b174dca8316e6
congle-engineer <congle-engineer@gmail.com>
2025-04-17 15:19:03 +0700

    break-after, break-before, break-inside

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="space-y-8">
+      <div className="columns-2">
+        <p className="break-inside-avoid-column">
+          Well, let me tell you something, ...
+        </p>
+        <p>Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
+      </div>
+
+      <div className="columns-2">
+        <p className="break-after-column">
+          Well, let me tell you something, ...
+        </p>
+        <p>Sure, go ahead, laugh...</p>
+        <p>Maybe we can live without...</p>
+        <p>Look. If you think this is...</p>
+      </div>
+
       <div className="columns-3 space-y-4 gap-4">
         <Image
           className="aspect-3/2"
