4f19bdabfae37bf9deb8576687d95479a54cd010
congle-engineer <congle-engineer@gmail.com>
2025-04-19 20:28:08 +0700

    display

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,55 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="space-y-8">
+      <p>
+        When controlling the flow of text, using the CSS property{" "}
+        <span className="inline bg-gray-500 text-white px-2 py-1">
+          display: inline
+        </span>{" "}
+        will cause the text inside the element to wrap normally
+      </p>
+
+      <p>
+        While using the property{" "}
+        <span className="inline-block bg-gray-500 text-white px-2 py-1 w-1/2">
+          display: inline-block
+        </span>{" "}
+        will wrap the element to prevent the text inside from extending beyond
+        its parent
+      </p>
+
+      <p>
+        Lastly, using the property{" "}
+        <span className="block bg-gray-500 text-white px-2 py-1">
+          display: block
+        </span>{" "}
+        will put the element on its own line and fill its parent
+      </p>
+
+      <div className="p-4">
+        <div className="flow-root">
+          <div className="my-4">
+            Well, let me tell you something, funny boy. Y'know that little
+            stamp, the one that says "New York Public Library"? Well that may
+            not mean anything to you, but that means a lot to me. One whole hell
+            of a lot.
+          </div>
+        </div>
+        <div className="flow-root">
+          <div className="my-4">
+            Sure, go ahead, laugh if you want to. I've seen your type before:
+            Flashy, making the scene, flaunting convention. Yeah, I know what
+            you're thinking. What's this guy making such a big stink about old
+            library books? Well, let me give you a hint, junior.
+          </div>
+        </div>
+      </div>
+
+      <div className="flow-root bg-gray-100">
+        <div className="float-left bg-blue-300 w-1/2">Float Left</div>
+        <div className="float-right bg-yellow-300 w-1/2">Float Right</div>
+      </div>
+
       <div className="box-border size-32 border-4 p-4">
         <p>A simple text here</p>
       </div>
