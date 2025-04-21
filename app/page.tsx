3f71456c4ed6748195ca0cdbc007582f3aa14054
congle-engineer <congle-engineer@gmail.com>
2025-04-21 17:23:55 +0700

    float

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="space-y-8">
+      <article>
+        <Image
+          className="float-none"
+          src="/img/mountain3.jpg"
+          alt="Mountain 3"
+          width={200}
+          height={200}
+        />
+        <p>
+          Maybe we can live without libraries, people like you and me. Maybe.
+          Sure, we're too old to change the world, but what about that kid,
+          sitting down, opening a book, right now, in a branch at the local
+          library and finding drawings of pee-pees and wee-wees on the Cat in
+          the Hat and the Five Chinese Brothers? Doesn't HE deserve better?
+          Look. If you think this is about overdue fines and missing books,
+          you'd better think again. This is about that kid's right to read a
+          book without getting his mind warped! Or: maybe that turns you on,
+          Seinfeld; maybe that's how y'get your kicks. You and your good-time
+          buddies.
+        </p>
+      </article>
+
       <p>
         When controlling the flow of text, using the CSS property{" "}
         <span className="inline bg-gray-500 text-white px-2 py-1">
