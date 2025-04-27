ea80c005a3f48f42f1f3151b51e1aee118574f95
congle-engineer <congle-engineer@gmail.com>
2025-04-27 11:23:56 +0700

    overflow, overscroll

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,28 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="space-y-8">
-      <div className="w-100 h-100">
-        <Image
-          className="size-24 object-center"
-          src="/img/snow-mountains.jpg"
-          alt="Snow Mountains"
-          width={1000}
-          height={1000}
-        />
+      <div className="w-100 h-50 overflow-auto overscroll-contain">
+        Well, let me tell you something, funny boy. Y'know that little stamp,
+        the one that says "New York Public Library"? Well that may not mean
+        anything to you, but that means a lot to me. One whole hell of a lot.
+        Sure, go ahead, laugh if you want to. I've seen your type before:
+        Flashy, making the scene, flaunting convention. Yeah, I know what you're
+        thinking. What's this guy making such a big stink about old library
+        books? Well, let me give you a hint, junior. Maybe we can live without
+        libraries, people like you and me. Maybe. Sure, we're too old to change
+        the world, but what about that kid, sitting down, opening a book, right
+        now, in a branch at the local library and finding drawings of pee-pees
+        and wee-wees on the Cat in the Hat and the Five Chinese Brothers?
+        Doesn't HE deserve better?
       </div>
+
+      <Image
+        className="size-24 object-center"
+        src="/img/snow-mountains.jpg"
+        alt="Snow Mountains"
+        width={1000}
+        height={1000}
+      />
 
       <Image
         className="h-48 w-96 object-cover"
