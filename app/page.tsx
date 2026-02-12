b2843eec6a0d5da66c1f444ddfec63c4b43c0d5f
congle-engineer <congle.engineer@gmail.com>
2026-02-12 12:54:40 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Submit
+      </button>
     </div>
   );
 }
