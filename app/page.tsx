2219d38a465dde7133392f34c616d8650c3d2583
congle-engineer <congle.engineer@gmail.com>
2026-03-04 11:58:52 +0700

    overscroll-behavior

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <div className="m-8 h-48 w-96 overflow-auto overscroll-auto">
+        <p>
+          Well, let me tell you something, funny boy. Y'know that little stamp,
+          the one that says "New York Public Library"? Well that may not mean
+          anything to you, but that means a lot to me. One whole hell of a lot.
+          Sure, go ahead, laugh if you want to. I've seen your type before:
+          Flashy, making the scene, flaunting convention. Yeah, I know what
+          you're thinking. What's this guy making such a big stink about old
+          library books? Well, let me give you a hint, junior. Maybe we can live
+          without libraries, people like you and me. Maybe. Sure, we're too old
+          to change the world, but what about that kid, sitting down, opening a
+          book, right now, in a branch at the local library and finding drawings
+          of pee-pees and wee-wees on the Cat in the Hat and the Five Chinese
+          Brothers? Doesn't HE deserve better?
+        </p>
+      </div>
+      <hr />
+
       <div className="overflow-visible">
         <h1>Overflow Visible</h1>
       </div>
