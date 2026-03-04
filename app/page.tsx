02923c63ced224fcee69534c18a5bf2447326e1e
congle-engineer <congle.engineer@gmail.com>
2026-03-04 07:48:53 +0700

    clear

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,36 @@ export default function Home() {
     <div>
       <article>
         <Image
+          className="float-left"
+          src="/img/snow-mountains.jpg"
+          alt="Snow Mountains"
+          width={200}
+          height={100}
+        />
+        <Image
+          className="float-right"
+          src="/img/green-mountains.jpg"
+          alt="Green Mountains"
+          width={400}
+          height={400}
+        />
+        <p className="clear-both">
+          Maybe we can live without libraries, people like you and me. Maybe.
+          Sure, we're too old to change the world, but what about that kid,
+          sitting down, opening a book, right now, in a branch at the local
+          library and finding drawings of pee-pees and wee-wees on the Cat in
+          the Hat and the Five Chinese Brothers? Doesn't HE deserve better?
+          Look. If you think this is about overdue fines and missing books,
+          you'd better think again. This is about that kid's right to read a
+          book without getting his mind warped! Or: maybe that turns you on,
+          Seinfeld; maybe that's how y'get your kicks. You and your good-time
+          buddies.
+        </p>
+      </article>
+      <hr />
+
+      <article>
+        <Image
           className="float-right"
           src="/img/snow-mountains.jpg"
           alt="Mountains"
